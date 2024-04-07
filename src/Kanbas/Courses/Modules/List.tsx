cc1921c33214eff7a75a3677abf21515594b90da
latteiscoding <zjg.elaine@gmail.com>
2024-04-07 11:53:15 -0400

    change modules[0] to module

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -43,7 +43,7 @@ function ModuleList() {
   const modules = useSelector((state: KanbasState) => state.modulesReducer.modules);
   const module = useSelector((state: KanbasState) => state.modulesReducer.module);
   const dispatch = useDispatch();
-  const [selectedModule, setSelectedModule] = useState(modules[0]); //set the first module as the default selected module
+  const [selectedModule, setSelectedModule] = useState(module); //set the first module as the default selected module
 
   
   return (

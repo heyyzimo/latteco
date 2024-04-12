291f7865f4c96a51d7166260bf1ab98e5ce18ad1
latteiscoding <zjg.elaine@gmail.com>
2024-04-12 14:33:12 -0400

    fix courses not displaying

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -12,7 +12,8 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 
 
 function Kanbas() {
-   const [courses, setCourses] = useState<any[]>([]);
+  
+   const [courses, setCourses] = useState<any[]>(db.courses);
    const COURSES_API = `${API_BASE}/api/courses`;
   const findAllCourses = async () => {
     const response = await axios.get(COURSES_API);
